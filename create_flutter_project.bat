2f4ac62d80df644dee2d1fe02a97c1b4cbd30736
DanielBarragan96 <ie702554@iteso.mx>
2020-09-11 22:19:59 -0500

    changed misc_dir

--- create_flutter_project.bat
+++ create_flutter_project.bat
@@ -1,8 +1,8 @@
 cd "%CD%"
-set misc_dir=%CD%
+set misc_dir=E:\Projects\Android\misc
 set /P name="Project name: "
 call flutter create %name%
-cd %name%
+call cd %name%
 call python %misc_dir%\update_sdk.py
 call xcopy %misc_dir%\open_vscode.bat %CD%\
 code .
