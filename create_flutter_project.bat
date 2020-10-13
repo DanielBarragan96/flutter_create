2f555673ccb4e128c05b2ad6320724925e403026
DanielBarragan96 <ie702554@iteso.mx>
2020-10-13 07:37:37 -0500

    Update create_flutter_project.bat

--- create_flutter_project.bat
+++ create_flutter_project.bat
@@ -2,7 +2,7 @@ cd "%CD%"
 set misc_dir=E:\Projects\Android\misc
 set /P name="Project name: "
 call flutter create %name%
-call cd %name%
+cd %name%
 call python %misc_dir%\update_sdk.py
 call xcopy %misc_dir%\open_vscode.bat %CD%\
 code .
